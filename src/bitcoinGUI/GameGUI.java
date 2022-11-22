8cd7d327dd34bd3ad73d4c5d83bda8e741c44326
hardticket <nickyhellatrigger@gmail.com>
2022-11-22 17:30:54 +0000

    Setting up the gui, due to lost work since my git wasnt committing the changes i did

--- src/bitcoinGUI/GameGUI.java
+++ src/bitcoinGUI/GameGUI.java
@@ -1,5 +1,17 @@
 package bitcoinGUI;
 
-public class GameGUI{
+import javax.swing.*;
+
+public class GameGUI extends JFrame {
+    private static GameGUI gui;
+        private static void createWindow(){
+            //Creating and setting up the game window
+                gui = new GameGUI();
+                gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                gui.setTitle("Bitcoin Catcher");
+                gui.setResizable(false);
+                gui.setSize(900, 600);
+
+        }
 
 }
---
+++ src/bitcoinGUI/Main.java
@@ -0,0 +1,9 @@
+package bitcoinGUI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}

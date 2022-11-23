9bc827935bff26dc7acf3302e422664ad2d3cb14
hardticket <nickyhellatrigger@gmail.com>
2022-11-23 20:20:00 +0000

    Created a blank gui!

--- src/bitcoinGUI/GameGUI.java
+++ src/bitcoinGUI/GameGUI.java
@@ -1,17 +1,33 @@
 package bitcoinGUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameGUI extends JFrame {
     private static GameGUI gui;
-        private static void createWindow(){
+        public static void createWindow(){
             //Creating and setting up the game window
                 gui = new GameGUI();
                 gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
                 gui.setTitle("Bitcoin Catcher");
                 gui.setResizable(false);
                 gui.setSize(900, 600);
+                gui.setLocationRelativeTo(null);
+                gui.setVisible(true);
 
         }
+        public static void onRepaint (Graphics g){
+
+        }
+        private static class GameField extends JPanel{
+            @Override
+            protected void paintComponent (Graphics g){
+                super.paintComponent(g);
+                onRepaint(g);
+                repaint();
+
+            }
+        }
+
 
 }
--- src/bitcoinGUI/Main.java
+++ src/bitcoinGUI/Main.java
@@ -1,9 +1,11 @@
 package bitcoinGUI;
 
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+        GameGUI.createWindow();
     }
+
 }
